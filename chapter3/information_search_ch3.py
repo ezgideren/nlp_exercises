9c207653657e3e2fb41c874a8909ff69b14421e7
ezgi deren <ezgideren@ezgis-MacBook-Pro.local>
2023-07-26 22:52:16 +0200

    txt-preprocessing started

--- chapter3/information_search_ch3.py
+++ chapter3/information_search_ch3.py
@@ -1,8 +1,12 @@
 import os
 import codecs
+import nltk
+from nltk import word_tokenize
+nltk.download('punkt')
 
 def read_documents():
-    f = open("chapter3/cisi/CISI.ALL")
+    f = open("chapte
+             r3/cisi/CISI.ALL")
     merged = ""
 #Results of merging the field identifier with its content.
 
@@ -20,18 +24,32 @@ def read_documents():
     doc_id = ""
 
     for a_line in merged.split("\n"):
+        #Getting the articles id if written next to I.
         if a_line.startswith(".I"):
             doc_id = a_line.split(" ")[1].strip()
+        #Getting the article's content accordance with the article's id
         elif a_line.startswith(".X"):
             documents[doc_id] = content
             content = ""
             doc_id = ""
         else:
             content += a_line.strip()[3:] + " "
-
     f.close()
     return documents
 
+#Checking the size and the content
+# documents = read_documents()
+# print(len(documents))
+# print(documents.get("1"))
+
+def get_words(text):
+    word_list = [word for word in word_tokenize(text.lower())]
+    return word_list
+
 documents = read_documents()
-print(len(documents))
-print(documents.get("1"))
+doc_words = {}
+qry_words = {}
+for doc_id in documents.keys():
+    doc_words[doc_id] = get_words(documents.get(doc_id))
+for qry_id in queries.keys():
+    
