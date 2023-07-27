daa37c2a20a8c5c3afe4d9df1718dad28df043ea
ezgi deren <ezgideren@ezgis-MacBook-Pro.local>
2023-07-27 19:26:02 +0200

    queries added

--- chapter3/information_search_ch3.py
+++ chapter3/information_search_ch3.py
@@ -5,8 +5,7 @@ from nltk import word_tokenize
 nltk.download('punkt')
 
 def read_documents():
-    f = open("chapte
-             r3/cisi/CISI.ALL")
+    f = open("chapter3/cisi/CISI.ALL")
     merged = ""
 #Results of merging the field identifier with its content.
 
@@ -37,6 +36,34 @@ def read_documents():
     f.close()
     return documents
 
+def read_queries():
+    f = open("chapter3/cisi/CISI.QRY")
+    merged = ""
+
+    for a_line in f.readlines():
+        if a_line.startswith("."):
+            merged += "\n" + a_line.strip()
+        else:
+            merged += " " + a_line.strip()
+
+    queries = {}
+
+    content = ""
+    qry_id = ""
+
+    for a_line in merged.split("\n"):
+        if a_line.startswith(".I"):
+            if not content == "":
+                queries[qry_id] = content
+                content = ""
+                qry_id = ""
+            qry_id = a_line.split(" ")[1].strip()
+        elif a_line.startswith(".W") or a_line.startswith(".T"):
+            content += a_line.strip()[3:] + " "
+    queries[qry_id] = content
+    
+    f.close()
+    return queries
 #Checking the size and the content
 # documents = read_documents()
 # print(len(documents))
@@ -52,4 +79,3 @@ qry_words = {}
 for doc_id in documents.keys():
     doc_words[doc_id] = get_words(documents.get(doc_id))
 for qry_id in queries.keys():
-    
