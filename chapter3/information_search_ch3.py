aa1024121de6a33cc9e08d35ed1ac285f345fe53
ezgi deren <ezgideren@ezgis-MacBook-Pro.local>
2023-07-27 20:45:30 +0200

    simple boolean search algorithm added

--- chapter3/information_search_ch3.py
+++ chapter3/information_search_ch3.py
@@ -4,6 +4,8 @@ import nltk
 from nltk import word_tokenize
 nltk.download('punkt')
 
+
+#Dictionary for documents
 def read_documents():
     f = open("chapter3/cisi/CISI.ALL")
     merged = ""
@@ -36,6 +38,7 @@ def read_documents():
     f.close()
     return documents
 
+#Dictionary for queries
 def read_queries():
     f = open("chapter3/cisi/CISI.QRY")
     merged = ""
@@ -64,18 +67,67 @@ def read_queries():
     
     f.close()
     return queries
+
+#Dictionary for mappings
+#Which queries correspond to which documents.
+def read_mappings():
+    f = open("chapter3/cisi/CISI.REL")
+
+    mappings = {}
+
+    for a_line in f.readlines():
+        voc = a_line.strip().split()
+        key = voc[0].strip()
+        current_value = voc[1].strip()
+        value = []
+        if key in mappings.keys():
+            value = mappings.get(key)
+            value.append(current_value)
+            mappings[key] = value
+
+    f.close
+    return mappings
+
 #Checking the size and the content
 # documents = read_documents()
 # print(len(documents))
 # print(documents.get("1"))
 
+#Preprocessing the data in documents and queries
 def get_words(text):
     word_list = [word for word in word_tokenize(text.lower())]
     return word_list
 
 documents = read_documents()
+queries = read_queries()
+
 doc_words = {}
 qry_words = {}
 for doc_id in documents.keys():
     doc_words[doc_id] = get_words(documents.get(doc_id))
 for qry_id in queries.keys():
+    qry_words[qry_id] = get_words(queries.get(qry_id))
+
+# print(len(doc_words))
+# print(doc_words.get("1"))
+# print(len(doc_words.get("1")))
+# print(len(qry_words))
+# print(qry_words.get("1"))
+# print(len(qry_words.get("1")))
+
+#Simple boolean search algorithm
+def retrieve_documents(doc_words, query):
+    docs = []
+    for doc_id in doc_words.keys():
+        found = False
+        i = 0
+        while i < len(query) and not found:
+            word = query[i]
+            if word in doc_words.get(doc_id):
+                docs.append(doc_id)
+                found = True
+            else:
+                i +=1
+    return docs
+
+
